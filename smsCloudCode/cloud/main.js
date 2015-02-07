f45bc476ecfcd69dff2960a3389b3cea36b47f39
Dylan Jeffers <CaneEnabled@gmail.com>
2015-02-07 03:06:11 -0500

    Added texting functionality

--- smsCloudCode/cloud/main.js
+++ smsCloudCode/cloud/main.js
@@ -1,6 +1,29 @@
 
 // Use Parse.Cloud.define to define as many cloud functions as you want.
 // For example:
+
 Parse.Cloud.define("hello", function(request, response) {
   response.success("Hello world!");
 });
+
+Parse.Cloud.define("sendSMS", function(request, response){
+
+    var client = require('twilio')('AC40dc454f2c503e6f4578d9165b313a96', '0d7fd1fa9d97222b263cba34206b563e'); 
+     
+    client.sendSms({  
+      //to: request.params.number,
+      to: request.params.number,
+      from:'+12564292078', 
+      body: request.params.msgbody
+
+     }, function(err, responseData) { 
+       if (err) {
+         console.log(err);
+       } else {
+            alert("TEXT SENT!"); 
+         // console.log(responseData.from); 
+         // console.log(responseData.body);
+       }
+     }
+     );
+    });
