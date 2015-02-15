53e74c87e03c722956a113c5271ff4731f99cfd9
Michael Piazza <michaelpiazza@dhcp-130-58-193-252.swarthmore.edu>
2015-02-14 20:52:12 -0500

    table working...

--- smsCloudCode/public/js/discover.js
+++ smsCloudCode/public/js/discover.js
@@ -1,27 +1,26 @@
 $(function(){ 
-
 	Parse.initialize("kg3Jvwzxa0HSaJR0J1hVf4B23qqUi9UkwTM9ykH9", "WJ7hKtik8cAtR4e8fdMRTlR7wzBqGNoueRUZMeoV");
 	var currentUserName;
-
+  
 	//Still need to authenticate user
 
   // This returns all of the groups the current user is a member of
-    function getMemberGroups(){
-      var currentUser = Parse.User.current();
-      return currentUser.get("groups");
-    }
+  function getMemberGroups(){
+    var currentUser = Parse.User.current();
+    return currentUser.get("groups");
+  }
     //this logout function needs to also disassociate User.currert()
-    function logout(){
-      Parse.User.logOut();
-      window.location = "/index.html";
-    }
+  function logout(){
+    Parse.User.logOut();
+    window.location = "/index.html";
+  }
 
-    function makeTopBar(user){
-      var userText = document.getElementById("user-name");
-      userText.innerHTML = user ;
-    }
+  function makeTopBar(user){
+    var userText = document.getElementById("user-name");
+    userText.innerHTML = user ;
+  }
 
-    function addToGroup(groupName){
+  function addToGroup(groupName){
 		var currentUser = Parse.User.current();
 		currentUser.add("groups", groupName);
 		currentUser.save().then(function(currentUser){
@@ -53,19 +52,7 @@ $(function(){
                 }
                 //Switch cell to ✓, and join group on parse
                 cell.innerHTML = "✓";
-
                 addToGroup(group);
-                //add to queue group
-                // Parse.Cloud.run("joinGroup",{
-                //   "username": Parse.User.current().get("username"),
-                //   "newgroup": group
-                // },{
-                //   success: function(result){
-                //     alert(result);
-                //   },
-                //   error: function(error){
-                //     }
-                //   });
                 };
               };
 
@@ -73,38 +60,69 @@ $(function(){
       }
   }
 
+    // function makeTable(){
+    //   var groupsArray;
+    //   var tableLength;
+    //   var memberGroups = getMemberGroups();
+    //   // var groupsTable = $("#groups-table");
+    //   var groupsTable = document.getElementById("groups-table");
+    //   Parse.Cloud.run("getGroups",{},{
+    //     success: function(result){
+    //       groupsArray = result;
+    //       // Make the table
+    //       for(var i = 0; i < groupsArray.length; i++){
+    //         group = groupsArray[i];
+    //         var match = false;
+    //         // Make row, then cell[0] (stores group name)
+    //         var row = groupsTable.insertRow(i);
+    //         var nameCell = row.insertCell(0);
+    //         // Set the cell[0] text to the group name
+    //         var joinedCell = row.insertCell(1);
+    //         nameCell.innerHTML = group;
+    //         if(isInGroup(group)){
+    //           joinedCell.innerHTML="✓";
+    //         }
+    //         else{
+    //           joinedCell.innerHTML="+";
+    //         }
+    //       }
+    //       addRowHandlers();
+    //     },
+    //     error: function(error){
+    //       //didn't work yo
+    //     }
+    //   });
+    // }
     function makeTable(){
-      var groupsArray;
-      var tableLength;
       var memberGroups = getMemberGroups();
-      // var groupsTable = $("#groups-table");
-      var groupsTable = document.getElementById("groups-table");
-      Parse.Cloud.run("getGroups",{},{
-        success: function(result){
+      var groupsArray;
+      var groupsTable = $("#groups-table");
+
+      // Add our global event listener for the entire table:
+      groupsTable.on("click","tr", function(event){
+        // Grab the row 
+        var row = $(this);
+        // Grabs the text of the group
+        var grp = row.children().first().text();
+        // add to group
+        addToGroup(grp);
+        
+      });
+
+      // Grab the groups:
+      Parse.Cloud.run("getGroups",{},{}).then(
+        function(result){
           groupsArray = result;
-          // Make the table
+        }, 
+        function (error){
+          alert("Found an error.")
+
+        }).then(function(obj){
           for(var i = 0; i < groupsArray.length; i++){
             group = groupsArray[i];
-            var match = false;
-            // Make row, then cell[0] (stores group name)
-            var row = groupsTable.insertRow(i);
-            var nameCell = row.insertCell(0);
-            // Set the cell[0] text to the group name
-            var joinedCell = row.insertCell(1);
-            nameCell.innerHTML = group;
-            if(isInGroup(group)){
-              joinedCell.innerHTML="✓";
-            }
-            else{
-              joinedCell.innerHTML="+";
-            }
+            groupsTable.append('<tr><td>' + group + '</td>' + '<td>' + isInGroup(group) + '</td></tr>');
           }
-          addRowHandlers();
-        },
-        error: function(error){
-          //didn't work yo
-        }
-      });
+        }, function(error){});
     }
 
     //This function returns a bool, according to whether or the user in a specific group
@@ -115,11 +133,8 @@ $(function(){
         return true;
       }
       return false;
-    	//eventuall call this function
     }
     //authenticateUser();
     makeTable();
     //var groupArray = makeTable();
-
-
-});
+    });
